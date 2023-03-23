4491ff347844d063b64594427b123b95eb56816b
Saloni0412 <pshashikantb@gmail.com>
2023-03-22 22:34:44 -0400

    this commit adds screenshot and link for deployed app in README file

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# password-generator
+# Password Generator
 
 ## Description
 
-This project was created to build a randomly generated password. This makes it easier for people to generate password with their requirements. Main purpose of making this project was to ease the stress for creating a password with specific required criteria. This tool gives to a password with few clicks.
+This project was created to build a randomly generated password. This makes it easier for people to generate password with their requirements. Main purpose of making this project was to ease the stress for creating a password with specific required criteria. This tool gives a password with few clicks.
 
 ## Installation
 
@@ -12,12 +12,15 @@ N/A
 
 This is a website which generates random passwords with the given criteria that user enters. When one click on generate password button, they will be prompted for the required length of the password. They can select from 8 to 128 character long password. Then after they will be prompted for few more criteria which includes having uppercase, lowercase, numeric value and special characters. One needs to atleast select one of the mentioned criteria. Once password is created it will be printed on the screen. Screenshot of the generated password is linked below.
 
+![photo of the website](./Assets/saloni0412.github.io_password-generator_.png)
 ## Credits
 
-https://www.w3schools.com/js/ - I used this to get some clear view on the javascript properties
+[WS Schools](https://www.w3schools.com/js/) - I used this to get some clear view on the javascript properties
 
 ## License
 
 MIT License
 
 ## Link 
+
+[Deployed website](https://saloni0412.github.io/password-generator/)
--- script.js
+++ script.js
@@ -1,5 +1,76 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var inputLength;
+
+
+
+
+// Calling function generatePassword to create a random password
+function generatePassword() {
+
+  // defining all the characters in different variables for the questions for criteria
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numericValue = "1234567890"
+  var specialCharcters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "
+  var newCriteria = ""
+
+  // calling function to input the lenght of the password
+  userInput();
+  function userInput() {
+    inputLength = prompt("Enter the lenght of the password between 8 - 128 characters", "only numeric form");
+
+    if (inputLength < 8 || inputLength > 128 || isNaN(inputLength)) {
+      alert("is not a valid value. Please select lenght between 8 to 128");
+      userInput();
+    }
+  }
+
+  // promping for the creteria 
+  var inputLower = confirm("Would you like lower case in your password");
+
+  if (inputLower == true) {
+    newCriteria += lowerCase;
+    console.log(newCriteria);
+  }
+
+  var inputUpper = confirm("Would you like upper case in your password");
+
+  if (inputUpper == true) {
+    newCriteria += upperCases;
+    console.log(newCriteria);
+  }
+
+  var inputNumeric = confirm("Would you like numerical value in your password");
+
+  if (inputNumeric == true) {
+    newCriteria += numericValue;
+    console.log(newCriteria);
+  }
+
+  var inputSymbol = confirm("Would you like special characters in your password");
+
+  if (inputSymbol == true) {
+    newCriteria += specialCharcters;
+    console.log(newCriteria);
+  }
+
+  // if statement if they have selected cancle in all the criteria
+  if (inputLower == false && inputUpper == false && inputNumeric == false &&
+    inputSymbol == false) {
+    window.alert("Please select atleast one of the following criteria");
+    generatePassword();
+  }
+
+  var password ="";
+
+  for (let i = 0; i < inputLength; i++) {
+    var newRandom = Math.floor(Math.random() * newCriteria.length);
+    password += newCriteria[newRandom];
+  }
+
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,69 +83,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Calling function generatePassword to create a random password
-function generatePassword() {
-
-// defining all the characters in different variables for the questions for criteria
-lowerCase = "abcdefghijklmnopqrstuvwxyz"
-upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numericValue = "1234567890"
-specialCharcters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ " 
-newCriteria = ""
-
-// calling function to input the lenght of the password
-userInput();
-function userInput() {
-var inputLength = prompt("Enter the lenght of the password between 8 - 128 characters", "only numeric form");
-
-if(inputLength < 8 || inputLength > 128 || isNaN(inputLength)) {
-  alert( "is not a valid value. Please select lenght between 8 to 128");
-  userInput();
-}
-}
-
-// promping for the creteria 
-var inputLower = confirm("Would you like lower case in your password");
-
-if(inputLower == true){
-  newCriteria += lowerCase;
-  console.log(newCriteria);
-}
-
-var inputUpper = confirm("Would you like upper case in your password");
-
-if(inputUpper == true){
-  newCriteria += upperCases;
-  console.log(newCriteria);
-}
-
-var inputNumeric = confirm("Would you like numerical value in your password");
-
-if(inputNumeric == true){
-  newCriteria += numericValue;
-  console.log(newCriteria);
-}
-
-var inputSymbol = confirm("Would you like special characters in your password");
-
-if(inputSymbol == true){
-  newCriteria += specialCharcters;
-  console.log(newCriteria);
-}
-
-// if statement if they have selected cancle in all the criteria
-if(inputLower == false && inputUpper == false && inputNumeric == false && 
-  inputSymbol == false) {
-    window.alert("Please select atleast one of the following criteria");
-    generatePassword();
-  }
-
-  
-  for(let i = 1; i < inputLength; i++) {
-    var newRandom = Math.floor(Math.random() * newCriteria.length);
-     password += newCriteria[newRandom];
-   }
-
-return password;
-}
